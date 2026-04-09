69bf4a1d94f7fcb23a2901435e424937b428b95c
Weihao Li <llreczx@gmail.com>
2026-04-09 19:08:38 +1000

    unify the cov interface

--- torch_openreml/cov.py
+++ torch_openreml/cov.py
@@ -5,63 +5,88 @@ from abc import ABC, abstarctmethod
 class CovMatrix(ABC):
 
     @abstractmethod
-    def build(self, *args, **kwargs):
+    def build(self, grad=True):
         raise NotImplementedError
 
     @property
     def grad(self):
         if not hasattr(self, "_grad"):
-            raise RuntimeError(f"Call with grad=True first.")
+            raise RuntimeError(f"Call `build()` with grad=True first.")
         return self._grad
       
 class ScalarMatrix(CovMatrix):
-    def build(n, sigma2, grad=True):
+  
+    def __init__(self, n, sigma2):
+        self.n = n
+        self.sigma2 = sigma2
+        self.device = self.sigma2.device
+        self.dtype = self.sigma2.dtype
+        
+    def build(self, grad=True):
         if grad:
-            self._grad = [torch.eye(n, device=sigma2.device, dtype=sigma2.dtype)]
-        return sigma2 * torch.eye(n, device=sigma2.device, dtype=sigma2.dtype)
+            self._grad = [torch.eye(self.n, device=self.device, dtype=self.dtype)]
+        return self.sigma2 * torch.eye(self.n, device=self.device, dtype=self.dtype)
 
 class DiagMatrix(CovMatrix):
-    def build(sigma2, grad=True):
+  
+    def __init__(self, sigma2):
+        self.sigma2 = sigma2
+        self.n = sigma2.shape[0]
+        self.device = self.sigma2.device
+        self.dtype = self.sigma2.dtype
+        
+    def build(self, grad=True):
         if grad:
             m = []
-            for i in range(sigma2.shape[0]):
-                E = torch.zeros((n, n), device=sigma2.device, dtype=sigma2.dtype)
+            for i in range(self.n):
+                E = torch.zeros((self.n, self.n), device=self.device, dtype=self.dtype)
                 E[i, i] = 1.0
                 m.append(E)
             self._grad = m
-        return torch.diag(sigma2)
+        return torch.diag(self.sigma2)
 
 class CompoundSymmetricMatrix(CovMatrix):
-    def build(n, sigma2, rho, grad=True):
-        device = get_device(sigma2, rho)
-        dtype = get_dtype(sigma2, rho)
-        I = scalar_matrix(n, sigma2)
-        J = torch.ones((n, n), device=device, dtype=dtype)
+  
+    def __init__(self, n, sigma2, rho):
+        self.n = n
+        self.sigma2 = sigma2
+        self.rho = rho
+        self.device = get_device(sigma2, rho)
+        self.dtype = get_dtype(sigma2, rho)
+        
+    def build(self, grad=True):
+        I = scalar_matrix(self.n, self.sigma2)
+        J = torch.ones((self.n, self.n), device=self.device, dtype=self.dtype)
         cor = (1 - rho) * I + rho * J
         
         if grad:
             d_sigma2 = cor
-            d_rho = sigma2 * (J - I)
+            d_rho = self.sigma2 * (J - I)
             self._grad = [d_sigma2, d_rho]
             
-        return sigma2 * cor
+        return self.sigma2 * cor
 
 class AR1Matrix(CovMatrix):
-    def build(n, sigma2, rho, grad=True):
-        device = get_device(sigma2, rho)
-        dtype = get_dtype(sigma2, rho)
-        idx = torch.arange(n, device=device,dtype=dtype)
+  
+    def __init__(self, n, sigma2, rho):
+        self.n = n
+        self.sigma2 = sigma2
+        self.rho = rho
+        self.device = get_device(sigma2, rho)
+        self.dtype = get_dtype(sigma2, rho)
+        
+    def build(self, grad=True):
+        idx = torch.arange(self.n, device=self.device, dtype=self.dtype)
         diff = torch.abs(idx.unsqueeze(0) - idx.unsqueeze(1))
-        rho_power = rho ** diff
+        rho_power = self.rho ** diff
         
         if grad:
             d_sigma2 = rho_power
-            d_rho = sigma2 * diff * rho_power / (rho + 1e-6)
+            d_rho = self.sigma2 * diff * rho_power / (rho + 1e-6)
 
             self._grad = [d_sigma2, d_rho]
             
-        return sigma2 * rho_power
-      
-class BlockDiagMatrix(CovMatrix):
-    def build(blocks, grad=True):
-        torch.block_diag(*blocks)
+        return self.sigma2 * rho_power
+
+class KroneckerProductMatrix(CovMatrix):
+    pass 
