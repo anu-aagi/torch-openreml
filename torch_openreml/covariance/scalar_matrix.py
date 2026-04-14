d62c75fccf2ee65871dbfc5771bbe0faace62497
Weihao Li <llreczx@gmail.com>
2026-04-14 15:45:33 +1000

    Improve arg handle

---
+++ torch_openreml/covariance/scalar_matrix.py
@@ -0,0 +1,24 @@
+from torch_openreml.covariance.covariance_matrix import CovarianceMatrix
+
+class ScalarMatrix(CovarianceMatrix):
+  
+    def __init__(self, n, no_grad_index=None):
+        super().__init__(n, ["log_sigma"], no_grad_index)
+        
+    def _comptue_grad(self, v):
+        self.reset_grad()
+        if len(self.no_grad_index) == 0:
+            self._grad = [2 * v]
+            self._grad_names = self._param_names
+        
+    def build(self, params, grad=True):
+        param_dict = self.to_param_dict(params)
+        device, dtype = self.check_params(params)
+        
+        sigma2 = torch.exp(2 * param_dict["log_sigma"])
+        v = sigma2 * torch.eye(self.n, device=device, dtype=dtype)
+        
+        if grad:
+            self._compute_grad(v=v)
+        
+        return v
