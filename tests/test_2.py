f52bbe4174c7684be3f097a1c5f83be110a6e1a5
Weihao Li <llreczx@gmail.com>
2026-04-16 16:30:26 +1000

    Update personal test

--- tests/test_2.py
+++ tests/test_2.py
@@ -12,6 +12,7 @@ auto_grad = A.grad
 
 print(manual_grad)
 print(manual_grad == auto_grad)
+print(manual_grad.shape == auto_grad.shape)
 
 A = torch_openreml.covariance.DiagonalMatrix(3)
 A.build(torch.tensor([1.0, 2.0, 3.0]))
@@ -21,6 +22,7 @@ auto_grad = A.grad
 
 print(manual_grad)
 print(manual_grad == auto_grad)
+print(manual_grad.shape == auto_grad.shape)
 
 A = torch_openreml.covariance.CompoundSymmetricMatrix(3)
 A.build(torch.tensor([1.0, 2.0]))
@@ -30,6 +32,7 @@ auto_grad = A.grad
 
 print(manual_grad)
 print(manual_grad == auto_grad)
+print(manual_grad.shape == auto_grad.shape)
 print(A.trans_rho(torch.tensor([2.0])))
 
 A = torch_openreml.covariance.AR1Matrix(3)
@@ -40,5 +43,23 @@ auto_grad = A.grad
 
 print(manual_grad)
 print(manual_grad == auto_grad)
+print(manual_grad.shape == auto_grad.shape)
 print(A.trans_rho(torch.tensor([2.0])))
+
+A.build({"log_sigma": torch.tensor([1.0]), "scaled_rho": torch.tensor([2.0])})
+
+A = torch_openreml.covariance.IdentityMatrix(3)
+B = torch_openreml.covariance.ScalarMatrix(3)
+C = torch_openreml.covariance.DiagonalMatrix(3)
+D = torch_openreml.covariance.SumMatrix({"A": A, "B": B, "C": C})
+D.param_names
+
+D.build(torch.tensor([0.0, 1.0, 2.0, 3.0]))
+manual_grad = D.grad
+D.auto_grad(torch.tensor([0.0, 1.0, 2.0, 3.0]))
+auto_grad = D.grad
+
+print(manual_grad)
+print(manual_grad == auto_grad)
+print(manual_grad.shape == auto_grad.shape)
 
