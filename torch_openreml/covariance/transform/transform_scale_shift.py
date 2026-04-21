c249796d860aef14389c5b71b708b0b74ddea22e
Weihao Li <llreczx@gmail.com>
2026-04-22 01:17:57 +1000

    Add scale shift

---
+++ torch_openreml/covariance/transform/transform_scale_shift.py
@@ -0,0 +1,19 @@
+from torch_openreml.covariance.transform.transform import Transform
+
+class TransformScaleShift(Transform):
+
+    domain = "\u211D"
+    codomain = "\u211D"
+
+    def __init__(self, a, b=0.0):
+        self.a = a
+        self.b = b
+
+    def __call__(self, x):
+        return self.a * x + self.b
+
+    def inverse(self, x):
+        return (x - self.b) / self.a
+
+    def chain_rule_factor(self, x):
+        return 1.0 / self.a
