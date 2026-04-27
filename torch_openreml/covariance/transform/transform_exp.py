34cdf58d81bec4b4c0cde755e31329307d4bf51b
Weihao Li <llreczx@gmail.com>
2026-04-27 17:34:27 +1000

    Add exp trans

--- torch_openreml/covariance/transform/transform_exp.py
+++ torch_openreml/covariance/transform/transform_exp.py
@@ -3,51 +3,51 @@ import torch
 
 class TransformExp(Transform):
 
-    domain = "\u211D\u207A"
-    codomain = "\u211D"
+    domain = "\u211D"
+    codomain = "\u211D\u207A"
 
     def __init__(self):
         pass
     
     def __call__(self, x):
-        return torch.log(x)
+        return torch.exp(x)
     
     def inverse(self, x):
-        return torch.exp(x)
+        return torch.log(x)
     
     def chain_rule_factor(self, x):
         return torch.exp(x)
 
-class TransformLog2(Transform):
+class TransformExp2(Transform):
 
-    domain = "\u211D\u207A"
-    codomain = "\u211D"
+    domain = "\u211D"
+    codomain = "\u211D\u207A"
 
     def __init__(self):
         pass
 
     def __call__(self, x):
-        return torch.log2(x)
+        return torch.exp2(x)
 
     def inverse(self, x):
-        return torch.exp2(x)
+        return torch.log2(x)
 
     def chain_rule_factor(self, x):
         return torch.exp2(x) * torch.log(torch.tensor([2], dtype=x.dtype, device=x.device))
 
-class TransformLog10(Transform):
+class TransformExp10(Transform):
 
-    domain = "\u211D\u207A"
-    codomain = "\u211D"
+    domain = "\u211D"
+    codomain = "\u211D\u207A"
 
     def __init__(self):
         pass
 
     def __call__(self, x):
-        return torch.log10(x)
+        return torch.pow(10.0, x)
 
     def inverse(self, x):
-        return torch.pow(10.0, x)
+        return torch.log10(x)
 
     def chain_rule_factor(self, x):
         return torch.pow(10.0, x) * torch.log(torch.tensor([10], dtype=x.dtype, device=x.device))
