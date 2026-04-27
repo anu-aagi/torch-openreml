dbd50fd2d30dae875581aa91c19d240397e31099
Weihao Li <llreczx@gmail.com>
2026-04-27 17:35:16 +1000

    Add transform_exppow2

--- torch_openreml/covariance/transform/transform_exppow2.py
+++ torch_openreml/covariance/transform/transform_exppow2.py
@@ -2,18 +2,19 @@ from torch_openreml.covariance.transform.transform import Transform
 import torch
 
 
-class TransformSqrtLog(Transform):
-    domain = "\u211D\u2080\u207A"
-    codomain = "\u211D"
+class TransformExpPow2(Transform):
+
+    domain = "\u211D"
+    codomain = "\u211D\u2080\u207A"
 
     def __init__(self):
         pass
 
     def __call__(self, x):
-        return torch.log(x) / 2.0
+        return torch.exp(2.0 * x)
 
     def inverse(self, x):
-        return torch.exp(2.0 * x)
+        return torch.log(x) / 2.0
 
     def chain_rule_factor(self, x):
         return 2 * torch.exp(2 * x)
