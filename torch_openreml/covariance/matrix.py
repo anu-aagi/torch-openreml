c686d9e8d7c1363c59531f45b082fddf00951957
Weihao Li <llreczx@gmail.com>
2026-04-27 17:32:16 +1000

    Update mtrix

--- torch_openreml/covariance/matrix.py
+++ torch_openreml/covariance/matrix.py
@@ -8,17 +8,17 @@ class Matrix(ABC):
     _repr_single_line = True
   
     def __init__(self, shape, param_names, trans, no_grad_index=None):
-        self.check_shape(shape)
+        self._check_shape(shape)
         self._shape = tuple(shape or ())
         
         self.reset_grad()
-        self.check_no_grad_index(no_grad_index)
+        self._check_no_grad_index(no_grad_index)
         
-        self.check_param_names(param_names)
+        self._check_param_names(param_names)
         self._param_names = param_names
         self._num_params = len(param_names)
 
-        self.check_trans(trans)
+        self._check_trans(trans)
         self._trans = trans
 
         self._no_grad_index = list(set(no_grad_index or []))
@@ -31,17 +31,17 @@ class Matrix(ABC):
         if (index is None) == (param_name is None):
             raise ValueError("Provide exactly one of 'index' or 'param_name'!")
         
-        if (param_name is None):
-            if (not isinstance(index, list)):
+        if param_name is None:
+            if not isinstance(index, list):
                 index = [index]
-            self.check_no_grad_index(index)
+            self._check_no_grad_index(index)
             self._no_grad_index = list(set(index))
         
-        if (index is None):
-            if (not isinstance(param_name, list)):
-                param_names = [param_name]
+        if index is None:
+            if not isinstance(param_name, list):
+                param_name = [param_name]
             index_map = {name: i for i, name in enumerate(self._param_names)}
-            index = [index_map[name] for n in param_names]
+            index = [index_map[name] for name in param_name]
             self._no_grad_index = list(set(index))
             
     def from_param_dict(self, param_dict):
@@ -66,10 +66,37 @@ class Matrix(ABC):
             raise ValueError(f"Expected {len(self._param_names)} parameters, got {len(params)}!")
         
         return {name: tensor for name, tensor in zip(self.param_names, params)}
+
+    def trans_params(self, params):
+        if len(self.trans) == 0:
+            return params
+
+        if len(self.trans) == 1:
+            return self.trans[0](params)
+        else:
+            return torch.stack([self.trans[i](x) for i, x in enumerate(params)])
+
+    def trans_chain_rule_factor(self, params):
+        if len(self.trans) == 0:
+            return 1.0
+
+        if len(self.trans) == 1:
+            return self.trans[0].chain_rule_factor(params)
+        else:
+            return torch.stack([self.trans[i].chain_rule_factor(x) for i, x in enumerate(params)])
       
     def auto_grad(self, params):
         jacobian = torch.func.jacrev(partial(self.build, grad=False))(params)
         jacobian = jacobian.permute(2, 0, 1)
+
+        if len(self.no_grad_index) == self._num_params:
+            self._grad = None
+            self._grad_names = self.param_names
+        else:
+            mask = torch.ones(self.num_params, dtype=torch.bool)
+            mask[self.no_grad_index] = False
+            self._grad = jacobian[mask]
+            self._grad_names = [name for i, name in enumerate(self.param_names) if i not in self.no_grad_index]
 
     @abstractmethod
     def build(self, params, grad=True):
@@ -79,7 +106,7 @@ class Matrix(ABC):
         return self.build(theta, grad=True)
       
     def map_theta_to_dv(self, theta):
-        if self.grad is None and len(self.no_grad_index) < len(self.num_params):
+        if self.grad is None and len(self.no_grad_index) < self.num_params:
             self.build(grad=True)
         return self.grad
     
@@ -87,7 +114,7 @@ class Matrix(ABC):
         if not isinstance(n, int):
             raise TypeError("'n' must be an int!")
           
-    def check_shape(self, shape):
+    def _check_shape(self, shape):
         if shape is None:
             return
         
@@ -99,15 +126,18 @@ class Matrix(ABC):
         if not all([isinstance(p, int) and p > 0 for p in shape]):
             raise TypeError("All elements of 'shape' must be positive int!")
 
-    def check_trans(self, trans):
+    def _check_trans(self, trans):
         if isinstance(trans, (list, tuple)):
             for t in trans:
                 if not isinstance(t, Transform):
                     raise TypeError("'trans' must be a list of Transform objects!")
         else:
             raise TypeError("'trans' must be a list of Transform objects!")
+
+        if len(trans) not in (0, 1, self._num_params):
+            raise ValueError(f"'trans' must be 0, 1, or 'num_params'!")
       
-    def check_no_grad_index(self, no_grad_index):
+    def _check_no_grad_index(self, no_grad_index):
         if no_grad_index is not None:
             if any(idx < 0 or idx >= self._num_params for idx in no_grad_index):
                 raise ValueError("Parameter index outside range!")
@@ -126,7 +156,9 @@ class Matrix(ABC):
         
         return params.device, params.dtype
     
-    def check_param_names(self, param_names):
+    def _check_param_names(self, param_names):
+        if not isinstance(param_names, (list, tuple)) or not all(isinstance(x, str) for x in param_names):
+            raise TypeError("'param_names' must be a list or tuple of strings!")
         if len(param_names) != len(set(param_names)):
             raise ValueError(f"Parameter names must be unique!")
 
