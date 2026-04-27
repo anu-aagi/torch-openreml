9fb4766354d6312063f223fc6bcc8e4c05f7fb81
Weihao Li <llreczx@gmail.com>
2026-04-27 17:35:36 +1000

    Add transform sigmoid

--- torch_openreml/covariance/transform/transform_sigmoid.py
+++ torch_openreml/covariance/transform/transform_sigmoid.py
@@ -1,19 +1,19 @@
 from torch_openreml.covariance.transform.transform import Transform
 import torch
 
-class TransformLogit(Transform):
+class TransformSigmoid(Transform):
 
-    domain = "(0, 1)"
-    codomain = "\u211D\u2080\u207A"
+    domain = "\u211D\u2080\u207A"
+    codomain = "(0, 1)"
 
     def __init__(self):
         pass
 
     def __call__(self, x):
-        return torch.logit(x)
+        return torch.sigmoid(x)
 
     def inverse(self, x):
-        return torch.sigmoid(x)
+        return torch.logit(x)
 
     def chain_rule_factor(self, x):
         sigmoid = torch.sigmoid(x)
