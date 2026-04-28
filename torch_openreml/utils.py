ebc908b612e86f0b81f63e1eab121a8709963ceb
Weihao Li <llreczx@gmail.com>
2026-04-28 14:48:40 +1000

    Add som utils

--- torch_openreml/utils.py
+++ torch_openreml/utils.py
@@ -1,3 +1,5 @@
+import torch
+
 def map_z_g_r_to_v(z, g, r):
     return z @ g @ z.T + r
   
@@ -22,3 +24,70 @@ def get_dtype(*args):
             raise ValueError(f"Dtype mismatch at arg {i}: {t.dtype} != {dtype}")
     
     return dtype
+
+def numeric_to_design_matrix(*args, dtype=None, device=None):
+
+    if len(args) == 0:
+        raise ValueError("At least one input is required.")
+
+    cols = []
+    n = None
+
+    dtype = dtype or torch.float32
+    device = device or "cpu"
+
+    for i, x in enumerate(args):
+
+        if torch.is_tensor(x):
+            x = x.to(dtype=dtype, device=device)
+        elif isinstance(x, (list, tuple)):
+            x = torch.tensor(x, dtype=dtype, device=device)
+        else:
+            raise TypeError("'x' must be either a tensor or a list/tuple of values!")
+
+        if len(x.shape) == 2:
+            x.unsqueeze_(0)
+
+        if n is None:
+            n = x.shape[0]
+        elif x.shape[0] != n:
+            raise ValueError(f"Inconsistent lengths at argument {i}: expected {n}, got {x.shape[0]}")
+
+        cols.append(x)
+
+    return torch.stack(cols, dim=1)
+
+def categorical_to_design_matrix(x, levels=None, drop_first=False, dtype=None, device=None):
+
+    dtype = dtype or torch.float32
+    device = device or "cpu"
+
+    if levels is None:
+        levels = sorted(set(x))
+
+    if len(levels) != len(set(x)):
+        raise ValueError("'levels' must match the number of unique values in 'x'!")
+
+    level_to_idx = {lev: i for i, lev in enumerate(levels)}
+
+    idx = torch.tensor([level_to_idx[v] for v in x], device=device, dtype=torch.long)
+
+    z = torch.nn.functional.one_hot(idx, num_classes=len(levels)).to(dtype=dtype)
+
+    if drop_first:
+        z = z[:, 1:]
+
+    return z
+
+def augment(*args):
+    return torch.cat(args, dim=1)
+
+def interaction(*args, sep="\u22C8"):
+    if len(args) == 0:
+        raise ValueError("At least one input is required.")
+
+    for i, arg in enumerate(args):
+        if not isinstance(arg, (list, tuple)):
+            raise TypeError(f"Argument {i} is not list/tuple of strings!")
+
+    return [sep.join(parts) for parts in zip(*args)]
