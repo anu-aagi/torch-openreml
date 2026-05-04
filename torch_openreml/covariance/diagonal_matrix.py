2a582b7bb87371311f5be04932a1e8d7aaf3780a
Weihao Li <llreczx@gmail.com>
2026-05-04 15:04:05 +1000

    Refactor DiagonalMatrix to the new API

--- torch_openreml/covariance/diagonal_matrix.py
+++ torch_openreml/covariance/diagonal_matrix.py
@@ -8,29 +8,27 @@ class DiagonalMatrix(Matrix):
         param_names = param_names or [f"sigma^2_{i}" for i in range(n)]
         trans = trans or [TransformExpPow2()]
         super().__init__((n, n), param_names, trans, no_grad_index)
-        
-    def _compute_grad(self, params, device, dtype):
-        self.reset_grad()
-        
-        if len(self.no_grad_index) == self.num_params:
-            return
-        
-        self._grad = torch.zeros(self.shape[0], self.shape[0], self.shape[0], device=device, dtype=dtype)
-        idx = torch.arange(self.shape[0], device=device)
-        self._grad[idx, idx, idx] = self.trans_chain_rule_factor(params)
-        
-        mask = torch.ones(self.shape[0], dtype=torch.bool, device=device)
-        mask[self.no_grad_index] = False
-        
-        self._grad = self._grad[mask]
-        self._grad_names = [name for i, name in enumerate(self.param_names) if i not in self.no_grad_index]
-        
-    def build(self, params, grad=True):
-        params = self.from_param_dict(params)
-        device, dtype = self.check_params(params)
+
+    def __call__(self, params):
         sigma2 = self.trans_params(params)
         
-        if grad:
-            self._compute_grad(params=params, device=device, dtype=dtype)
-        
         return torch.diag(sigma2)
+
+    def manual_grad(self, params):
+        if len(self.no_grad_index) == self.num_params:
+            return None, []
+
+        device, dtype = self.check_params(params)
+
+        grad = torch.zeros(self.shape[0], self.shape[0], self.shape[0], device=device, dtype=dtype)
+        idx = torch.arange(self.shape[0], device=device)
+        grad[idx, idx, idx] = self.trans_grad(params)
+
+        mask = torch.ones(self.shape[0], dtype=torch.bool, device=device)
+        mask[self.no_grad_index] = False
+
+        grad = grad[mask]
+        grad_names = [name for i, name in enumerate(self.param_names) if i not in self.no_grad_index]
+
+        return grad, grad_names
+
