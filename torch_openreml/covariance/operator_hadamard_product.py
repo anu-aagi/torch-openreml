818e3baff0ecfab51a15204a3e4143fdd3d4c20b
Weihao Li <llreczx@gmail.com>
2026-05-04 15:06:42 +1000

    Refactor HadamardProduct to new API.

--- torch_openreml/covariance/operator_hadamard_product.py
+++ torch_openreml/covariance/operator_hadamard_product.py
@@ -1,6 +1,7 @@
 from torch_openreml.covariance.operator import Operator
 import torch
 
+
 class HadamardProduct(Operator):
 
     def __init__(self, operands):
@@ -9,32 +10,51 @@ class HadamardProduct(Operator):
 
         super().__init__(None, operands)
 
-    def build(self, params, grad=True):
-        v_groups, grad_groups, grad_name_groups = self.build_operands(params, grad)
+    def _get_or_build_intermediates(self, params):
+        cache = self.get_intermediates(params)
 
-        a = v_groups[0]
-        b = v_groups[1]
-        v = a * b
+        if cache is None:
+            v_groups = self.build_operands(params)
 
-        if grad:
-            self.reset_grad()
+            a = v_groups[0]
+            b = v_groups[1]
+            v = a * b
 
-            grad_list = []
-            da = grad_groups[0]
-            db = grad_groups[1]
+            cache = {"a": a, "b": b, "v": v}
 
-            if da is not None:
-                da = da * b
-                grad_list.append(da)
-                self._grad_names.extend(grad_name_groups[0])
+            self.set_intermediates(params, cache)
 
-            if db is not None:
-                db = a * db
-                grad_list.append(db)
-                self._grad_names.extend(grad_name_groups[1])
+        return cache
 
-            if len(grad_list) > 0:
-                self._grad = torch.cat(grad_list)
-
+    def __call__(self, params):
+        cache = self._get_or_build_intermediates(params)
+        v = cache["v"]
         self._shape = tuple(v.shape)
+
         return v
+
+    def manual_grad(self, params):
+        grad_groups, grad_name_groups = self.operands_grad(params)
+
+        cache = self._get_or_build_intermediates(params)
+        a = cache["a"]
+        b = cache["b"]
+
+        grad = []
+        grad_names = []
+
+        da = grad_groups[0]
+        if da is not None:
+            grad.append(da * b)
+            grad_names.extend(grad_name_groups[0])
+
+        db = grad_groups[1]
+        if db is not None:
+            grad.append(a * db)
+            grad_names.extend(grad_name_groups[1])
+
+        if len(grad) > 0:
+            grad = torch.cat(grad)
+            return grad, grad_names
+        else:
+            return None, []
