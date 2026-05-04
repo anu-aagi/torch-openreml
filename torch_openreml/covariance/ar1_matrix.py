65af4aebf7cf355cf59df29f4c2f702a8556aa81
Weihao Li <llreczx@gmail.com>
2026-05-04 15:03:16 +1000

    Refactor AR1Matrix to the new API

--- torch_openreml/covariance/ar1_matrix.py
+++ torch_openreml/covariance/ar1_matrix.py
@@ -8,43 +8,52 @@ class AR1Matrix(Matrix):
         param_names = param_names or ["sigma^2", "rho"]
         trans = trans or [TransformExpPow2(), TransformChain([TransformSigmoid(), TransformScaleShift(2.0, -1.0)])]
         super().__init__((n, n), param_names, trans, no_grad_index)
-      
-    def _compute_grad(self, params, rho, sigma2, diff, rho_power):
-        self.reset_grad()
-        
-        if len(self.no_grad_index) == self.num_params:
-            return
-        
-        grad = []
 
-        chain_rule_factor = self.trans_chain_rule_factor(params)
+    def _get_or_build_intermediates(self, params):
+        cache = self.get_intermediates(params)
 
-        if 0 not in self.no_grad_index:
-            grad.append(chain_rule_factor[0] * rho_power)
-            self._grad_names.append(self.param_names[0])
-        
-        if 1 not in self.no_grad_index:
-            rho = torch.sign(rho) * torch.clamp(rho.abs(), min=1e-6)
-            d_rho = chain_rule_factor[1] * sigma2 * diff * rho_power / rho
-            d_rho.fill_diagonal_(0.0)
-            grad.append(d_rho)
-            self._grad_names.append(self.param_names[1])
-            
-        self._grad = torch.stack(grad)
-        
-    def build(self, params, grad=True):
-        params = self.from_param_dict(params)
-        device, dtype = self.check_params(params)
-        sigma2, rho = self.trans_params(params)
-      
-        idx = torch.arange(self.shape[0], device=params.device, dtype=params.dtype)
-        diff = torch.abs(idx.unsqueeze(0) - idx.unsqueeze(1))
-        
-        rho_power = rho ** diff
-        
-        v = sigma2 * rho_power
-        
-        if grad:
-            self._compute_grad(params=params, rho=rho, sigma2=sigma2, diff=diff, rho_power=rho_power)
+        if cache is None:
+            device, dtype = self.check_params(params)
+            sigma2, rho = self.trans_params(params)
+
+            idx = torch.arange(self.shape[0], device=device, dtype=dtype)
+            diff = torch.abs(idx.unsqueeze(0) - idx.unsqueeze(1))
+
+            rho_power = rho ** diff
+
+            cache = {"sigma2": sigma2, "rho": rho, "diff": diff, "rho_power": rho_power}
+            self.set_intermediates(params, cache)
+
+        return cache
+
+    def __call__(self, params):
+        cache = self._get_or_build_intermediates(params)
+        v = cache["sigma2"] * cache["rho_power"]
             
         return v
+
+    def manual_grad(self, params):
+        if len(self.no_grad_index) == self.num_params:
+            return None, []
+
+        cache = self._get_or_build_intermediates(params)
+
+        grad = []
+        grad_names = []
+
+        trans_grad = self.trans_grad(params)
+
+        if 0 not in self.no_grad_index:
+            grad.append(trans_grad[0] * cache["rho_power"])
+            grad_names.append(self.param_names[0])
+
+        if 1 not in self.no_grad_index:
+            scaled_rho = torch.sign(cache["rho"]) * torch.clamp(cache["rho"].abs(), min=1e-6)
+            d_rho = trans_grad[1] * cache["sigma2"] * cache["diff"] * cache["rho_power"] / scaled_rho
+            d_rho.fill_diagonal_(0.0)
+            grad.append(d_rho)
+            grad_names.append(self.param_names[1])
+
+        grad = torch.stack(grad)
+
+        return grad, grad_names
