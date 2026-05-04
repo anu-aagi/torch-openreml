51e6789039179979211480b58575c199a28eb89b
Weihao Li <llreczx@gmail.com>
2026-05-04 15:08:04 +1000

    Update test_2.py for new API

--- tests/test_2.py
+++ tests/test_2.py
@@ -2,48 +2,39 @@ import torch_openreml
 import torch
 
 A = torch_openreml.covariance.ScalarMatrix(3)
-A.build(torch.tensor([1.0]))
-manual_grad = A.grad
-A.auto_grad(torch.tensor([1.0]))
-auto_grad = A.grad
-
-print(manual_grad)
+x = torch.tensor([1.0])
+A(x)
+manual_grad = A.manual_grad(x)[0]
+auto_grad = A.auto_grad(x)[0]
 print(torch.allclose(manual_grad, auto_grad))
 print(manual_grad.shape == auto_grad.shape)
 
 A = torch_openreml.covariance.DiagonalMatrix(3)
-A.build(torch.tensor([1.0, 2.0, 3.0]))
-manual_grad = A.grad
-A.auto_grad(torch.tensor([1.0, 2.0, 3.0]))
-auto_grad = A.grad
-
-print(manual_grad)
+x = torch.tensor([1.0, 2.0, 3.0])
+A(x)
+manual_grad = A.manual_grad(x)[0]
+auto_grad = A.auto_grad(x)[0]
 print(torch.allclose(manual_grad, auto_grad))
 print(manual_grad.shape == auto_grad.shape)
 
 A = torch_openreml.covariance.CompoundSymmetricMatrix(3)
-A.build(torch.tensor([1.0, 2.0]))
-manual_grad = A.grad
-A.auto_grad(torch.tensor([1.0, 2.0]))
-auto_grad = A.grad
-
-print(manual_grad)
+x = torch.tensor([1.0, 2.0])
+A(x)
+manual_grad = A.manual_grad(x)[0]
+auto_grad = A.auto_grad(x)[0]
 print(torch.allclose(manual_grad, auto_grad))
 print(manual_grad.shape == auto_grad.shape)
-print(A.trans_params(torch.tensor([1.0, 2.0])))
 
 A = torch_openreml.covariance.AR1Matrix(3)
-A.build(torch.tensor([1.0, 2.0]))
-manual_grad = A.grad
-A.auto_grad(torch.tensor([1.0, 2.0]))
-auto_grad = A.grad
-
-print(manual_grad)
+x = torch.tensor([1.0, 2.0])
+A(x)
+manual_grad = A.manual_grad(x)[0]
+auto_grad = A.auto_grad(x)[0]
 print(torch.allclose(manual_grad, auto_grad))
 print(manual_grad.shape == auto_grad.shape)
-print(A.trans_params(torch.tensor([1.0, 2.0])))
 
-A.build({"sigma^2": torch.tensor([1.0]), "rho": torch.tensor([2.0])})
+A({"sigma^2": torch.tensor([1.0]), "rho": torch.tensor([2.0])})
+
 
 A = torch.eye(3, dtype=torch.float32, device=torch.device("cpu"))
 B = torch_openreml.covariance.ScalarMatrix(3)
@@ -52,26 +43,26 @@ D = torch_openreml.covariance.Sum({"A": A, "B": B, "C": C})
 D
 D.param_names
 
-D.build(torch.tensor([0.0, 1.0, 2.0, 3.0]))
-manual_grad = D.grad
-D.auto_grad(torch.tensor([0.0, 1.0, 2.0, 3.0]))
-auto_grad = D.grad
-
+x = torch.tensor([0.0, 1.0, 2.0, 3.0])
+D(x)
+manual_grad = D.manual_grad(x)[0]
+auto_grad = D.auto_grad(x)[0]
 print(manual_grad)
 print(torch.allclose(manual_grad, auto_grad))
 print(manual_grad.shape == auto_grad.shape)
-print(D.grad_names)
+print(D.manual_grad(x)[1])
 
 D.build_operands(torch.tensor([0.0, 1.0, 2.0, 3.0]))
+D.operands_grad(torch.tensor([0.0, 1.0, 2.0, 3.0]))
 
 E = torch_openreml.covariance.Sum({"D.1": D, "D.2": D})
 E
 E.param_names
 
-E.build(torch.tensor([0.0, 1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0]))
-manual_grad = E.grad
-E.auto_grad(torch.tensor([0.0, 1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0]))
-auto_grad = E.grad
+x = torch.tensor([0.0, 1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0])
+E(x)
+manual_grad = E.manual_grad(x)[0]
+auto_grad = E.auto_grad(x)[0]
 
 print(manual_grad)
 print(torch.allclose(manual_grad, auto_grad))
@@ -82,20 +73,19 @@ G = torch_openreml.covariance.ScalarMatrix(3)
 S = torch_openreml.covariance.LinearPropagation({"Z": Z, "G": G})
 S
 S.param_names
-S.build(torch.tensor([1.0]))
-manual_grad = S.grad
-S.auto_grad(torch.tensor([1.0]))
-auto_grad = S.grad
+x = torch.tensor([1.0])
+S(x)
+manual_grad = S.manual_grad(x)[0]
+auto_grad = S.auto_grad(x)[0]
 
 print(manual_grad)
 print(torch.allclose(manual_grad, auto_grad))
 print(manual_grad.shape == auto_grad.shape)
-print(S.grad_names)
+print(S.manual_grad(x)[1])
 
 A = torch_openreml.covariance.DiagonalMatrix(4)
 B = torch.eye(2)
 C = torch_openreml.covariance.KroneckerProduct({"A": A, "B": B})
-C.build(torch.tensor([0.0, 1.0, 2.0, 3.0]))
-C.grad
+C(torch.tensor([0.0, 1.0, 2.0, 3.0]))
+C.manual_grad(torch.tensor([0.0, 1.0, 2.0, 3.0]))
 C.auto_grad(torch.tensor([0.0, 1.0, 2.0, 3.0]))
-C.grad
