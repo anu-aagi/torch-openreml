0714f8b4e04723fca2f1fbd77c5991e907b4e182
Weihao Li <llreczx@gmail.com>
2026-09-10 13:38:50 +1000

    Add post methods to init

--- torch_openreml/__init__.py
+++ torch_openreml/__init__.py
@@ -1,4 +1,5 @@
 from torch_openreml.marginal_reml import MarginalREML
+from torch_openreml.post import blue, blup, marginal_predict, marginal_residual, predict, residual, loglik
 import torch_openreml.utils
 import torch_openreml.covariance
 import torch_openreml.example_data
