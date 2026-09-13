acd14c3a16dfe14a16efe22cbc28d16abeca84c7
Weihao Li <llreczx@gmail.com>
2026-09-13 23:02:45 +1000

    Remove global config from init

--- torch_openreml/__init__.py
+++ torch_openreml/__init__.py
@@ -1,6 +1,5 @@
 from torch_openreml.marginal_reml import MarginalREML
 from torch_openreml.post import blue, blup, marginal_predict, marginal_residual, predict, residual, loglik
-from torch_openreml.config import get_default_chunk_size, get_default_jacobian_method, jacobian_method, set_default_jacobian_method
 import torch_openreml.utils
 import torch_openreml.covariance
 import torch_openreml.example_data
